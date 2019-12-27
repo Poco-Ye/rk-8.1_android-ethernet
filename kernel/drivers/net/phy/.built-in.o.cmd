5c5dbdcc1af143a4a7031d6c4bcd6d58df118503
poco.ye <poco.ye@rock-chips.com>
2019-12-27 04:05:34 +0100

    add

---
+++ kernel/drivers/net/phy/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/net/phy/built-in.o :=  ./../prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-ld -EL    -r -o drivers/net/phy/built-in.o drivers/net/phy/libphy.o drivers/net/phy/rockchip.o 
---
+++ kernel/drivers/net/phy/.libphy.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/net/phy/libphy.o := ./../prebuilts/gcc/linux-x86/aarch64/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-ld -EL    -r -o drivers/net/phy/libphy.o drivers/net/phy/phy.o drivers/net/phy/phy_device.o drivers/net/phy/mdio_bus.o 
